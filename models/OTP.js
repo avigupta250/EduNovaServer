654909217cc9b2abd8ba7c47be809843e27244cb
avigupta250 <avinashkumarnav52@gmail.com>
2024-02-23 17:26:27 +0530

    pre save middleware for otp done

--- models/OTP.js
+++ models/OTP.js
@@ -1,22 +1,39 @@
-const mongoose=require('mongoose')
-
-
+const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		required: true,
-	},
-	otp: {
-		type: String,
-		required: true,
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-		expires: 60 * 5, 
-	},
+  email: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 60 * 5,
+  },
 });
 
+const sendVerificationEmail = async (email, otp) => {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verification Email from EduNova",
+      otp
+    );
 
-module.exports=mongoose.model("OTP",OTPSchema);
+    console.log("Email sent Successfully", mailResponse);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+OTPSchema.pre("save",async function(next){
+	await sendVerificationEmail(this.email,this.otp);
+	next();
+})
+
+module.exports = mongoose.model("OTP", OTPSchema);
