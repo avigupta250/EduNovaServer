58773fb51d08c3610984682f869f4c5782fde7d4
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-08 13:24:12 +0530

    origin

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		// origin:"https://edunova-frontend.vercel.app",
+		origin:"https://edunova-frontend.vercel.app",
 		origin:"http://localhost:3000",
 		credentials:true,
 	})
